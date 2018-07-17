d49dd1d54b85dcbf257e7ff610a0156341d70a5a
Cornelis Heuperman <cheuperman@gmail.com>
2018-07-17 18:17:11 +0100

    Add method to show all listings

--- lib/listing.rb
+++ lib/listing.rb
@@ -1,11 +1,24 @@
 require 'pg'
+
 class Listing
-  attr_reader :id
+  attr_reader :id, :name, :description, :price
   def initialize(id, name, description, price)
     @id = id
     @name = name
     @description = description
     @price = price
+  end
+
+  def self.all
+    if ENV['RACK_ENV'] == 'test'
+      @connection = PG.connect(dbname: 'airbnb_test')
+    else
+      @connection = PG.connect(dbname: 'airbnb')
+    end
+    result = @connection.exec('SELECT * FROM listings')
+    result.map do |listing|
+       Listing.new(listing['id'], listing['name'], listing['description'], listing['price'])
+     end
   end
 
   def self.create(name, description, price)
@@ -15,9 +28,9 @@ class Listing
       @connection = PG.connect(dbname: 'airbnb')
     end
     result = @connection.exec(
-      "INSERT INTO listings (name, description, price) " +
+      'INSERT INTO listings (name, description, price) ' +
       "VALUES ('#{name}', '#{description}', '#{price}')" +
-      " RETURNING id, name, description, price;"
+      ' RETURNING id, name, description, price;'
     ).first
     Listing.new(
       result['id'],
@@ -25,5 +38,9 @@ class Listing
       result['description'],
       result['price']
     )
+  end
+
+  def ==(other)
+    @id = other.id
   end
 end
--- spec/listing_spec.rb
+++ spec/listing_spec.rb
@@ -7,4 +7,16 @@ describe Listing do
       expect(listing.id).not_to be_nil
     end
   end
+
+  describe '.all' do
+    it 'returns all listings' do
+      listings = [
+        Listing.create('house1', 'nice garden', 100),
+        Listing.create('house2', 'two bedrooms', 50),
+        Listing.create('house3', 'terrace', 200)
+      ]
+
+      expect(Listing.all).to eq listings
+    end
+  end
 end
