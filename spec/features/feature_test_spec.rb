b318827c66e140e17d1c154ecb074cf92d44d575
bibi collins <bibicollins94@googlemail.com>
2018-07-17 18:32:28 +0100

    Added number value to price text box, and edited feature test

--- spec/features/feature_test_spec.rb
+++ spec/features/feature_test_spec.rb
@@ -1,16 +1,9 @@
 require './app.rb'
 
-feature 'Listings page ' do
-  scenario 'A user can view a listings page' do
-    visit('/listings')
-    click_button('Add listing')
-    expect(current_path).to eq '/listings/new'
-  end
-end
-
 feature 'New listings page ' do
   scenario 'A user can add a listing' do
-    visit('/listings/new')
+    visit('/listings')
+    click_button('Add listing')
     fill_in('name', with: 'Joe')
     fill_in('price', with: '50')
     fill_in('description', with: '3 bedroom flat to rent')
