5c3d334e3b4971739ce22f896dbf53c3a531771e
Jay Khan <jkhan992@gmail.com>
2018-07-19 14:53:26 +0100

    WIP booking confirmation flow

--- app.rb
+++ app.rb
@@ -27,5 +27,25 @@ end
 
   get '/users/new' do
     erb(:users_new)
-  end 
+  end
+
+  get '/users/my-listings' do
+    @listings_pending = Listing.all_open_requests
+    @listings_not_pending = Listing.all_closed_requests
+    erb(:users_my_listings)
+  end
+
+  post '/users/my-listings' do
+    Listing.book_listing(params['id'])
+    redirect '/users/my-listings'
+  end
+
+  post '/listings/:id/request' do
+    Listing.request_booking(params['id'])
+    redirect '/listings/:id/request'
+  end
+
+  get '/listings/:id/request' do
+    erb(:users_listing_request_confirmation)
+  end
 end
