019a3dc139beb98720ffb2e6e270ab4fde8fb1d2
Yvonne Tang <ytang604@gmail.com>
2018-07-20 11:41:15 +0100

    connect log in to model

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require 'sinatra/base'
 require 'uri'
 require 'sinatra/flash'
 require './lib/listing'
+require './lib/user'
 
 class BnB < Sinatra::Base
 
@@ -11,6 +12,17 @@ class BnB < Sinatra::Base
   get '/' do
     erb(:index)
 end
+
+post '/sessions/new' do
+   user = User.authenticate(params['email'], params['password'])
+   if user
+     redirect'/listings'
+   else
+     flash[:notice] = 'invalid email or password'
+     redirect('/')
+   end
+ end
+
   get '/listings' do
     @listings = Listing.all
     erb(:listings)
